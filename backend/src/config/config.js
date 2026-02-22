f8fc24f9323bbf65a142cdd591898be898089802
SakshAm240124 <167504695+SakshAm240124@users.noreply.github.com>
2026-02-22 06:52:36 +0530

    Use DATABASE_URL for production

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,20 +1,14 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
 
-module.exports = { sequelize, DataTypes };
+module.exports = sequelize;
